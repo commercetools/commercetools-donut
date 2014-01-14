b6a762c2b5764ba0571de1c61e786e94c8565683
Laura Luiz <laura.luiz@commercetools.de>
2014-01-14 18:13:33 +0100

    Fix variable names for setting configuration values.

--- adjust_config.sh
+++ adjust_config.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 PLAY_CONF_FILE="conf/application.conf"
-sed -i "s;^sphere.project=.*;sphere.project="${SPHERE_PROJECT}";g" "${PLAY_CONF_FILE}"
-sed -i "s;^sphere.clientId=.*;sphere.clientId="${SPHERE_CLIENT_ID}";g" "${PLAY_CONF_FILE}"
-sed -i "s;^sphere.clientSecret=.*;sphere.clientSecret="${SPHERE_CLIENT_SECRET}";g" "${PLAY_CONF_FILE}"
-sed -i "s;^sphere.clientSecret=.*;pactas.clientId="${PACTAS_CLIENT_ID}";g" "${PLAY_CONF_FILE}"
-sed -i "s;^sphere.clientSecret=.*;pactas.clientSecret="${PACTAS_CLIENT_SECRET}";g" "${PLAY_CONF_FILE}"
-sed -i "s;^application.secret=.*;application.secret="${PLAY_APP_SECRET}";g" "${PLAY_CONF_FILE}"
+sed -i "s;^sphere.project *=.*;sphere.project=\""${SPHERE_PROJECT}"\";g" "${PLAY_CONF_FILE}"
+sed -i "s;^sphere.clientId *=.*;sphere.clientId=\""${SPHERE_CLIENT_ID}"\";g" "${PLAY_CONF_FILE}"
+sed -i "s;^sphere.clientSecret *=.*;sphere.clientSecret=\""${SPHERE_CLIENT_SECRET}"\";g" "${PLAY_CONF_FILE}"
+sed -i "s;^pactas.clientId *=.*;pactas.clientId="${PACTAS_CLIENT_ID}";g" "${PLAY_CONF_FILE}"
+sed -i "s;^pactas.clientSecret *=.*;pactas.clientSecret="${PACTAS_CLIENT_SECRET}";g" "${PLAY_CONF_FILE}"
+sed -i "s;^application.secret *=.*;application.secret=\""${PLAY_APP_SECRET}"\";g" "${PLAY_CONF_FILE}"
 
 # we commit the changes in order to have them included in our push to heroku
 git config user.name "Travis CI"
