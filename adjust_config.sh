c4a4b7807a53fac280550ed039048abe4563fcb7
Laura Luiz <laura.luiz@commercetools.de>
2014-01-14 16:38:32 +0100

    Add encrypted Pactas client and secret.

--- adjust_config.sh
+++ adjust_config.sh
@@ -4,6 +4,8 @@ PLAY_CONF_FILE="conf/application.conf"
 sed -i "s;^sphere.project=.*;sphere.project="${SPHERE_PROJECT}";g" "${PLAY_CONF_FILE}"
 sed -i "s;^sphere.clientId=.*;sphere.clientId="${SPHERE_CLIENT_ID}";g" "${PLAY_CONF_FILE}"
 sed -i "s;^sphere.clientSecret=.*;sphere.clientSecret="${SPHERE_CLIENT_SECRET}";g" "${PLAY_CONF_FILE}"
+sed -i "s;^sphere.clientSecret=.*;pactas.clientId="${PACTAS_CLIENT_ID}";g" "${PLAY_CONF_FILE}"
+sed -i "s;^sphere.clientSecret=.*;pactas.clientSecret="${PACTAS_CLIENT_SECRET}";g" "${PLAY_CONF_FILE}"
 sed -i "s;^application.secret=.*;application.secret="${PLAY_APP_SECRET}";g" "${PLAY_CONF_FILE}"
 
 # we commit the changes in order to have them included in our push to heroku
