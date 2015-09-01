2fd882b83e2992b2c8836e8e324498149ac2d1dc
Markus Lamm <markus.lamm@commercetools.de>
2015-09-01 18:34:41 +0200

    remove old product references

--- app/controllers/BaseController.java
+++ app/controllers/BaseController.java
@@ -5,7 +5,6 @@ import io.sphere.client.exceptions.SphereException;
 import io.sphere.client.model.CustomObject;
 import io.sphere.client.shop.model.CartUpdate;
 import io.sphere.client.shop.model.LineItem;
-import io.sphere.client.shop.model.Product;
 import io.sphere.client.shop.model.Variant;
 import io.sphere.sdk.client.SphereClient;
 import io.sphere.sdk.products.ProductProjection;
@@ -26,18 +25,15 @@ public class BaseController extends Controller {
     public final static String ID_WEEKLY    = "pactas1";
 
     private final Sphere sphere;
-    private final Product product;
     private final CurrencyOperations currencyOps;
 
     private final SphereClient sphereClient;
     private final ProductProjection productProjection;
 
-    public BaseController(final Sphere sphere, final Configuration configuration, final Product product,
-                          final SphereClient sphereClient, final ProductProjection productProjection) {
+    public BaseController(final Sphere sphere, final Configuration configuration, final SphereClient sphereClient,
+                          final ProductProjection productProjection) {
         this.sphere = sphere;
-        this.product = product;
         this.currencyOps = CurrencyOperations.of(configuration);
-
         this.sphereClient = sphereClient;
         this.productProjection = productProjection;
     }
@@ -50,10 +46,6 @@ public class BaseController extends Controller {
         return currencyOps.currency();
     }
 
-    protected Product product() {
-        return product;
-    }
-
     protected SphereClient sphereClient() {
         return sphereClient;
     }
@@ -62,22 +54,11 @@ public class BaseController extends Controller {
         return productProjection;
     }
 
-    protected Optional<Variant> variant(final int variantId) {
-        return product().getVariants().byId(variantId);
+    protected java.util.Optional<ProductVariant> variant(final int variantId) {
+        return productProjection().getAllVariants().stream().filter(v -> v.getId().equals(variantId)).findFirst();
     }
 
-    protected Optional<Variant> variant(final String pactasId) {
-        for (final Variant variant : product().getVariants().asList()) {
-            if (pactasId.equals(variant.getString(ID_MONTHLY))
-                    || pactasId.equals(variant.getString(ID_TWO_WEEKS))
-                    || pactasId.equals(variant.getString(ID_WEEKLY))) {
-                return Optional.of(variant);
-            }
-        }
-        return Optional.absent();
-    }
-
-    protected java.util.Optional<ProductVariant> _variant(final String pactasId) {
+    protected java.util.Optional<ProductVariant> variant(final String pactasId) {
         for(final ProductVariant variant : productProjection().getAllVariants()) {
             final String monthly = variant.getAttribute(ID_MONTHLY).getValue(AttributeAccess.ofString());
             final String twoWeeks = variant.getAttribute(ID_TWO_WEEKS).getValue(AttributeAccess.ofString());
