96cf53a5d9686c4c1fd7999c84d2c25d616d4bc2
Markus Lamm <markus.lamm@commercetools.de>
2015-09-08 13:41:25 +0200

    fix MonetaryAmount formatting

--- app/utils/NewPriceUtils.java
+++ app/utils/NewPriceUtils.java
@@ -2,9 +2,10 @@ package utils;
 
 import io.sphere.sdk.carts.Cart;
 import io.sphere.sdk.products.Price;
+import io.sphere.sdk.utils.MoneyImpl;
 
 import javax.money.MonetaryAmount;
-import java.text.NumberFormat;
+import javax.money.format.MonetaryFormats;
 import java.util.Locale;
 import java.util.Optional;
 
@@ -14,19 +15,15 @@ public final class NewPriceUtils {
     }
 
     public static String format(final Price price) {
-        final MonetaryAmount amount = price.getValue();
-        return doFormat(amount);
+        return format(price.getValue());
     }
 
     public static String format(final Cart cart) {
-        final MonetaryAmount amount = cart.getTotalPrice();
-        return doFormat(amount);
+        return format(cart.getTotalPrice());
     }
 
-    private static String doFormat(MonetaryAmount amount) {
-        final String am = NumberFormat.getInstance(Locale.GERMANY).format(amount.getNumber().doubleValueExact());
-        final String currency = amount.getCurrency().getCurrencyCode();
-        return String.format("%s %s", am, currency);
+    private static String format(MonetaryAmount amount) {
+        return MonetaryFormats.getAmountFormat(Locale.GERMANY).format(MoneyImpl.of(amount));
     }
 
     public static double monetaryAmount(final Price price) {
