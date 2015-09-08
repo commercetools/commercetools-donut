6f0c38305bad4448a0387dcb106262371c965380
Markus Lamm <markus.lamm@commercetools.de>
2015-09-08 13:12:26 +0200

    document service interfaces

--- app/services/CartService.java
+++ app/services/CartService.java
@@ -9,17 +9,67 @@ import play.mvc.Http;
 
 import java.util.Optional;
 
+/**
+ * Business service that provides access to the SPHERE.IO Cart API.
+ */
 public interface CartService {
 
+    /**
+     * If there is no {@code SessionKeys.CART_ID} in the existing session, a new {@code Cart} will be created in the
+     * Sphere backend and its {@code SessionKeys.CART_ID} will be stored in the users {@code Http.Session session}.
+     * Otherwise it retrieves an existing Cart via its {@code SessionKeys.CART_ID}, but never null.
+     *
+     * @param session the Play session object, must not be null
+     * @return the newly created, or fetched {@code Cart} object, must not be null
+     */
     Cart getOrCreateCart(Http.Session session);
 
+    /**
+     * Resets an existing {@code Cart}.
+     * It performs two API calls, one to remove the Carts LineItems and another one to clear the {@code CustomObject}
+     * that holds the selected frequency of the product.
+     *
+     * @param cart the {@code Cart} object to clear, must not be null
+     * @return the updated {@code Cart} object as result from the update command to Sphere API, must not be null
+     */
     Cart clearCart(final Cart cart);
 
+
+    /**
+     * Adds the user selected {@code ProductVariant} of the {@code ProductProjection} with a given frequency to the
+     * users {@code Cart}.
+     *
+     * @param cart the user's current {@code Cart} object, must not be null
+     * @param product the {@code ProductProjection} to add, must not be null
+     * @param variant the selected {@code ProductVariant}, must not be null
+     * @param frequency the selected delivery frequency
+     */
     void setProductToCart(final Cart cart, final ProductProjection product, final ProductVariant variant, final int frequency);
 
+    /**
+     * Returns the value of the {@code CustomObject} named ShopKeys.FREQUENCY, that is bound to the {@code Cart}
+     * with the given cardId.
+     *
+     * @param cartId the identifier of the {@code Cart} object, the {@code CustomObject} is bound to
+     * @return the value of the {@code CustomObject}. If no {@code CustomObject} found, it returns 0
+     */
     int getFrequency(final String cartId);
 
+    /**
+     * Gets an optional, selected {@code ProductVariant} from the users {@code Cart}
+     *
+     * @param cart the {@code Cart} object to get the selected variant from, must not be null
+     * @return optional {@code ProductVariant}, maybe empty if there's no selection made
+     */
     Optional<ProductVariant> getSelectedVariant(final Cart cart);
 
+    /**
+     * Creates a {@code Cart} object with the required data from Pactas.
+     *
+     * @param product the selected  {@code ProductProjection}
+     * @param contract the {@code PactasContract}
+     * @param customer the {@code PactasCustomer}
+     * @return
+     */
     Cart createCartWithPactasInfo(final ProductProjection product, final PactasContract contract, final PactasCustomer customer);
 }
--- app/services/OrderService.java
+++ app/services/OrderService.java
@@ -3,7 +3,16 @@ package services;
 import io.sphere.sdk.carts.Cart;
 import io.sphere.sdk.orders.Order;
 
+/**
+ * Business service that provides access to the SPHERE.IO Order API.
+ */
 public interface OrderService {
 
+    /**
+     * Creates a new {@code Order} from an existing {@code Cart}.
+     *
+     * @param cart the {@code Cart} object to create the {@code Order} from, must not be null
+     * @return the newly created {@code Order}, must not be null
+     */
     Order createOrder(Cart cart);
 }
