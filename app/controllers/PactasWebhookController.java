3683ea280d84f587241f100b69acf3a27817ae5a
Markus Lamm <markus.lamm@commercetools.de>
2015-09-10 09:24:13 +0200

    adjust to 2.4 Futures

--- app/controllers/PactasWebhookController.java
+++ app/controllers/PactasWebhookController.java
@@ -1,76 +1,79 @@
-//package controllers;
-//
-//import io.sphere.sdk.carts.Cart;
-//import io.sphere.sdk.orders.Order;
-//import pactas.Pactas;
-//import pactas.PactasException;
-//import pactas.models.PactasContract;
-//import pactas.models.PactasCustomer;
-//import pactas.models.webhooks.Webhook;
-//import pactas.models.webhooks.WebhookAccountCreated;
-//import play.Configuration;
-//import play.Logger;
-//import play.mvc.Http;
-//import play.mvc.Result;
-//import services.CartService;
-//import services.OrderService;
-//import services.ProductService;
-//import utils.JsonUtils;
-//
-//import javax.inject.Inject;
-//import java.util.Optional;
-//
-//import static java.util.Objects.requireNonNull;
-//
-//public class PactasWebhookController extends BaseController {
-//
-//    private final Pactas pactas;
-//    private final OrderService orderService;
-//    private final CartService cartService;
-//    private final ProductService productService;
-//
-//    @Inject
-//    public PactasWebhookController(final Configuration configuration, final CartService cartService,
-//                                   final OrderService orderService, final ProductService productService,
-//                                   final Pactas pactas) {
-//        super(configuration);
-//        this.cartService = requireNonNull(cartService, "'cartService' must not be null");
-//        this.orderService = requireNonNull(orderService, "'orderService' must not be null");
-//        this.productService = requireNonNull(productService, "'productService' must not be null");
-//        this.pactas = requireNonNull(pactas, "'pactas' must not be null");
-//    }
-//
-//    /* Method called by Pactas every time an order must be placed (weekly, monthly...) */
-//    public Result createOrderFromSubscription() {
-//        Logger.debug("An order request has been received from Pactas...");
-//        final Optional<String> contractId = parseContractId(request());
-//        if (contractId.isPresent()) {
-//            try {
-//                final PactasContract contract = pactas.fetchContract(contractId.get()).get();
-//                Logger.debug("Fetched Pactas contract: {}", contract);
-//                final PactasCustomer customer = pactas.fetchCustomer(contract.getCustomerId()).get();
-//                Logger.debug("Fetched Pactas customer: {}", customer);
-//                final Cart cart = cartService.createCartWithPactasInfo(productService.getProduct().get(), contract, customer);
-//                final Order order = orderService.createOrder(cart);
-//                Logger.debug("Order created: {}", order);
-//                return ok();
-//            } catch (PactasException e) {
-//                Logger.error(e.getMessage(), e);
-//                return internalServerError();
-//            } catch (Exception e) {
-//                Logger.error(e.getMessage(), e);
-//            }
-//        }
-//        return badRequest();
-//    }
-//
-//    private Optional<String> parseContractId(final Http.Request request) {
-//        Logger.debug("Pactas webhook: " + request.body().asText());
-//        final Webhook webhook = JsonUtils.readObject(Webhook.class, request.body().asText());
-//        if (webhook instanceof WebhookAccountCreated) {
-//            return Optional.of(((WebhookAccountCreated) webhook).getContractId());
-//        } else {
-//            return Optional.empty();
-//        }
-//    }
-//}
+package controllers;
+
+import io.sphere.sdk.carts.Cart;
+import io.sphere.sdk.orders.Order;
+import pactas.Pactas;
+import pactas.PactasException;
+import pactas.models.PactasContract;
+import pactas.models.PactasCustomer;
+import pactas.models.webhooks.Webhook;
+import pactas.models.webhooks.WebhookAccountCreated;
+import play.Application;
+import play.Logger;
+import play.mvc.Http;
+import play.mvc.Result;
+import services.CartService;
+import services.OrderService;
+import services.ProductService;
+import utils.JsonUtils;
+
+import javax.inject.Inject;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.Objects.requireNonNull;
+
+public class PactasWebhookController extends BaseController {
+
+    private static final Logger.ALogger LOG = Logger.of(PactasWebhookController.class);
+
+    private final Pactas pactas;
+    private final OrderService orderService;
+    private final CartService cartService;
+    private final ProductService productService;
+
+    @Inject
+    public PactasWebhookController(final Application application, final CartService cartService,
+                                   final OrderService orderService, final ProductService productService,
+                                   final Pactas pactas) {
+        super(application);
+        this.cartService = requireNonNull(cartService, "'cartService' must not be null");
+        this.orderService = requireNonNull(orderService, "'orderService' must not be null");
+        this.productService = requireNonNull(productService, "'productService' must not be null");
+        this.pactas = requireNonNull(pactas, "'pactas' must not be null");
+    }
+
+    /* Method called by Pactas every time an order must be placed (weekly, monthly...) */
+    public Result createOrderFromSubscription() {
+        Logger.debug("An order request has been received from Pactas...");
+        final Optional<String> contractId = parseContractId(request());
+        if (contractId.isPresent()) {
+            try {
+                final PactasContract contract = pactas.fetchContract(contractId.get()).get(2000, TimeUnit.MILLISECONDS);
+                LOG.debug("Fetched Pactas contract: {}", contract);
+                final PactasCustomer customer = pactas.fetchCustomer(contract.getCustomerId()).get(2000, TimeUnit.MILLISECONDS);
+                LOG.debug("Fetched Pactas customer: {}", customer);
+                final Cart cart = cartService.createCartWithPactasInfo(productService.getProduct().get(), contract, customer);
+                final Order order = orderService.createOrder(cart);
+                LOG.debug("Order created: {}", order);
+                return ok();
+            } catch (PactasException e) {
+                LOG.error(e.getMessage(), e);
+                return internalServerError();
+            } catch (Exception e) {
+                LOG.error(e.getMessage(), e);
+            }
+        }
+        return badRequest();
+    }
+
+    private Optional<String> parseContractId(final Http.Request request) {
+        LOG.debug("Pactas webhook: " + request.body().asText());
+        final Webhook webhook = JsonUtils.readObject(Webhook.class, request.body().asText());
+        if (webhook instanceof WebhookAccountCreated) {
+            return Optional.of(((WebhookAccountCreated) webhook).getContractId());
+        } else {
+            return Optional.empty();
+        }
+    }
+}
--- app/pactas/PactasImpl.java
+++ app/pactas/PactasImpl.java
@@ -1,90 +1,90 @@
-//package pactas;
-//
-//import com.google.common.net.HttpHeaders;
-//import com.ning.http.client.Realm;
-//import pactas.models.Authorization;
-//import pactas.models.PactasContract;
-//import pactas.models.PactasCustomer;
-//import play.Configuration;
-//import play.Logger;
-//import play.libs.F;
-//import play.libs.ws.WS;
-//import play.mvc.Http;
-//import utils.JsonUtils;
-//
-//import javax.inject.Inject;
-//
-//public class PactasImpl implements Pactas {
-//    public static final String CONTENT_TYPE = "application/x-www-form-urlencoded";
-//
-//    private final Configuration configuration;
-//    private final String authUrl;
-//    private final String clientId;
-//    private final String clientSecret;
-//    private final F.Promise<Authorization> authorizationPromise;
-//
-//    @Inject
-//    public PactasImpl(final Configuration configuration) {
-//        this.configuration = configuration;
-//        this.authUrl = configuration.getString("pactas.auth");
-//        this.clientId = configuration.getString("pactas.clientId");
-//        this.clientSecret = configuration.getString("pactas.clientSecret");
-//        authorizationPromise = authenticate();
-//    }
-//
-//    public F.Promise<Authorization> fetchAuthorization() {
-//        return authorizationPromise;
-//    }
-//
-//    @Override
-//    public F.Promise<PactasContract> fetchContract(final String contractId) {
-//        String endpointUrl = configuration.getString("pactas.api.contracts") + contractId;
-//        return executeRequest(endpointUrl, PactasContract.class);
-//    }
-//
-//    @Override
-//    public F.Promise<PactasCustomer> fetchCustomer(final String customerId) {
-//        String endpointUrl = configuration.getString("pactas.api.customers") + customerId;
-//        return executeRequest(endpointUrl, PactasCustomer.class);
-//    }
-//
-//    /**
-//     * Sends the request to the provided Pactas endpoint and parses the response.
-//     * @param endpointUrl endpoint URL to Pactas.
-//     * @param clazz class against which the response will be deserialized.
-//     * @return Promise of response deserialized against the given class.
-//     * @throws PactasException when the request failed or the response could not be parsed.
-//     */
-//    private <T> F.Promise<T> executeRequest(final String endpointUrl, final Class<T> clazz) {
-//        return authorizationPromise.flatMap(authorization -> WS.url(endpointUrl)
-//                .setContentType(CONTENT_TYPE)
-//                .setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + authorization.getAccessToken())
-//                .get().map(response -> {
-//                    Logger.info(response.getBody());
-//                    if (response.getStatus() == Http.Status.OK) {
-//                        return JsonUtils.readObject(clazz, response.getBody());
-//                    } else {
-//                        throw new PactasException(response.getStatus(), response.getBody());
-//                    }
-//                }));
-//    }
-//
-//    /**
-//     * Sends the request to the auth Pactas endpoint and parses the access token.
-//     * @return Promise of the access token.
-//     */
-//    private F.Promise<Authorization> authenticate() {
-//        Logger.debug("Fetching pactas access token");
-//        return WS.url(authUrl)
-//                .setContentType(CONTENT_TYPE)
-//                .setAuth(clientId, clientSecret, Realm.AuthScheme.BASIC)
-//                .post("grant_type=client_credentials").map(response -> {
-//                    Logger.info(response.getBody());
-//                    if (response.getStatus() == Http.Status.OK) {
-//                        return JsonUtils.readObject(Authorization.class, response.getBody());
-//                    } else {
-//                        throw new PactasException(response.getStatus(), response.getBody());
-//                    }
-//                });
-//    }
-//}
+package pactas;
+
+import com.google.common.net.HttpHeaders;
+import pactas.models.Authorization;
+import pactas.models.PactasContract;
+import pactas.models.PactasCustomer;
+import play.Configuration;
+import play.Logger;
+import play.libs.F;
+import play.libs.ws.WS;
+import play.libs.ws.WSAuthScheme;
+import play.mvc.Http;
+import utils.JsonUtils;
+
+import javax.inject.Inject;
+
+public class PactasImpl implements Pactas {
+    public static final String CONTENT_TYPE = "application/x-www-form-urlencoded";
+
+    private final Configuration configuration;
+    private final String authUrl;
+    private final String clientId;
+    private final String clientSecret;
+    private final F.Promise<Authorization> authorizationPromise;
+
+    @Inject
+    public PactasImpl(final Configuration configuration) {
+        this.configuration = configuration;
+        this.authUrl = configuration.getString("pactas.auth");
+        this.clientId = configuration.getString("pactas.clientId");
+        this.clientSecret = configuration.getString("pactas.clientSecret");
+        authorizationPromise = authenticate();
+    }
+
+    public F.Promise<Authorization> fetchAuthorization() {
+        return authorizationPromise;
+    }
+
+    @Override
+    public F.Promise<PactasContract> fetchContract(final String contractId) {
+        String endpointUrl = configuration.getString("pactas.api.contracts") + contractId;
+        return executeRequest(endpointUrl, PactasContract.class);
+    }
+
+    @Override
+    public F.Promise<PactasCustomer> fetchCustomer(final String customerId) {
+        String endpointUrl = configuration.getString("pactas.api.customers") + customerId;
+        return executeRequest(endpointUrl, PactasCustomer.class);
+    }
+
+    /**
+     * Sends the request to the provided Pactas endpoint and parses the response.
+     * @param endpointUrl endpoint URL to Pactas.
+     * @param clazz class against which the response will be deserialized.
+     * @return Promise of response deserialized against the given class.
+     * @throws PactasException when the request failed or the response could not be parsed.
+     */
+    private <T> F.Promise<T> executeRequest(final String endpointUrl, final Class<T> clazz) {
+        return authorizationPromise.flatMap(authorization -> WS.url(endpointUrl)
+                .setContentType(CONTENT_TYPE)
+                .setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + authorization.getAccessToken())
+                .get().map(response -> {
+                    Logger.info(response.getBody());
+                    if (response.getStatus() == Http.Status.OK) {
+                        return JsonUtils.readObject(clazz, response.getBody());
+                    } else {
+                        throw new PactasException(response.getStatus(), response.getBody());
+                    }
+                }));
+    }
+
+    /**
+     * Sends the request to the auth Pactas endpoint and parses the access token.
+     * @return Promise of the access token.
+     */
+    private F.Promise<Authorization> authenticate() {
+        Logger.debug("Fetching pactas access token");
+        return WS.url(authUrl)
+                .setContentType(CONTENT_TYPE)
+                .setAuth(clientId, clientSecret, WSAuthScheme.BASIC)
+                .post("grant_type=client_credentials").map(response -> {
+                    Logger.info(response.getBody());
+                    if (response.getStatus() == Http.Status.OK) {
+                        return JsonUtils.readObject(Authorization.class, response.getBody());
+                    } else {
+                        throw new PactasException(response.getStatus(), response.getBody());
+                    }
+                });
+    }
+}
