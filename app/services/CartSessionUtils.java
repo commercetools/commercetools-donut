08247f8426ef4da7f3c2dde1ce97332e08486261
Markus Lamm <markus.lamm@commercetools.de>
2015-10-01 11:41:16 +0200

    handle session writing

--- app/services/CartSessionUtils.java
+++ app/services/CartSessionUtils.java
@@ -1,11 +1,17 @@
 package services;
 
+import io.sphere.sdk.carts.Cart;
+import play.Logger;
 import play.mvc.Http;
 import play.mvc.Http.Session;
 
 import java.util.Optional;
 
+import static java.util.Objects.requireNonNull;
+
 public final class CartSessionUtils {
+
+    private static final Logger.ALogger LOG = Logger.of(CartSessionUtils.class);
 
     public static Optional<Integer> getSelectedVariantIdFromSession(final Http.Session session) {
         try {
@@ -23,7 +29,21 @@ public final class CartSessionUtils {
         }
     }
 
-    public static void clearSession(final Session session) {
+    public static void writeCartSessionData(final Cart cart, final Session session) {
+        requireNonNull(cart);
+        requireNonNull(session);
+        session.put(SessionKeys.CART_ID, cart.getId());
+        if (!cart.getLineItems().isEmpty()) {
+            final long frequency = cart.getLineItems().get(0).getQuantity();
+            session.put(SessionKeys.FREQUENCY, String.valueOf(frequency));
+            final int variantId = cart.getLineItems().get(0).getVariant().getId();
+            session.put(SessionKeys.VARIANT_ID, String.valueOf(variantId));
+        }
+        LOG.debug("Wrote session data: {}", session.toString());
+    }
+
+
+    public static void clearProductFromSession(final Session session) {
         session.remove(SessionKeys.FREQUENCY);
         session.remove(SessionKeys.VARIANT_ID);
     }
