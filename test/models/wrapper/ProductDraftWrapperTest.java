ec90b21071b9ac46a2bcd0991b7e711811f0a9a9
Markus Lamm <markus.lamm@commercetools.de>
2015-10-14 17:49:02 +0200

    #38 removed mocks in ProductDraftWrapperTest

--- test/models/wrapper/ProductDraftWrapperTest.java
+++ test/models/wrapper/ProductDraftWrapperTest.java
@@ -5,7 +5,6 @@ import io.sphere.sdk.models.Reference;
 import io.sphere.sdk.products.ProductDraft;
 import io.sphere.sdk.producttypes.ProductType;
 import io.sphere.sdk.taxcategories.TaxCategory;
-import org.junit.Before;
 import org.junit.Test;
 import utils.JsonUtils;
 
@@ -13,28 +12,10 @@ import java.util.Locale;
 import java.util.UUID;
 
 import static org.assertj.core.api.StrictAssertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class ProductDraftWrapperTest {
 
     private static final String PRODUCT_JSON_RESOURCE = "data/product-draft.json";
-
-    private ProductType productType;
-    private TaxCategory taxCategory;
-
-    @Before
-    public void setUp() {
-        productType = mock(ProductType.class);
-        final String productTypeId = UUID.randomUUID().toString();
-        when(productType.toReference()).thenReturn(Reference.of("product-type", productTypeId, productType));
-        when(productType.getId()).thenReturn(productTypeId);
-
-        taxCategory = mock(TaxCategory.class);
-        final String taxCategoryId = UUID.randomUUID().toString();
-        when(taxCategory.toReference()).thenReturn(Reference.of("tax-category", taxCategoryId, taxCategory));
-        when(taxCategory.getId()).thenReturn(taxCategoryId);
-    }
 
     @Test
     public void testCreateProductDraft() throws Exception {
@@ -42,10 +23,16 @@ public class ProductDraftWrapperTest {
                 JsonUtils.readObjectFromResource(PRODUCT_JSON_RESOURCE, ProductDraftWrapper.class);
         assertThat(productDraftWrapper).isNotNull();
 
-        final ProductDraft productDraft = productDraftWrapper.createProductDraft(productType, taxCategory);
+        final Reference<ProductType> productTypeReference = Reference.of("product-type", UUID.randomUUID().toString());
+        final Reference<TaxCategory> taxCategoryReference = Reference.of("tax-category", UUID.randomUUID().toString());
+
+        final ProductDraft productDraft = productDraftWrapper.createProductDraft(productTypeReference, taxCategoryReference);
         assertThat(productDraft).isNotNull();
         assertThat(productDraft.getName()).isEqualTo(LocalizedString.of(Locale.ENGLISH, "Classic box"));
-        assertThat(productDraft.getProductType().getTypeId()).isEqualTo(productType.toReference().getTypeId());
+        assertThat(productDraft.getProductType().getTypeId()).isEqualTo(productTypeReference.getTypeId());
+        assertThat(productDraft.getProductType().getId()).isEqualTo(productTypeReference.getId());
+        assertThat(productDraft.getTaxCategory().getTypeId()).isEqualTo(taxCategoryReference.getTypeId());
+        assertThat(productDraft.getTaxCategory().getId()).isEqualTo(taxCategoryReference.getId());
         assertThat(productDraft.getSlug()).isEqualTo(LocalizedString.of(Locale.ENGLISH, "donut-box"));
         assertThat(productDraft.getMasterVariant().getSku()).isEqualTo("box6");
         assertThat(productDraft.getVariants().size()).isEqualTo(3);
