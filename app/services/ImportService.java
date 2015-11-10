e771eed10ba2ad42384a52fb8e357699551f3430
Markus Lamm <markus.lamm@commercetools.de>
2015-11-10 15:33:37 +0100

    remove custom type from ImportService

--- app/services/ImportService.java
+++ app/services/ImportService.java
@@ -1,12 +1,9 @@
 package services;
 
 import io.sphere.sdk.products.Product;
-import io.sphere.sdk.types.Type;
 import play.libs.F;
 
 public interface ImportService {
 
     F.Promise<Product> exportProductModel();
-
-    F.Promise<Type> exportCustomType();
 }
--- app/services/ImportServiceImpl.java
+++ app/services/ImportServiceImpl.java
@@ -14,14 +14,9 @@ import io.sphere.sdk.producttypes.commands.ProductTypeCreateCommand;
 import io.sphere.sdk.queries.PagedQueryResult;
 import io.sphere.sdk.taxcategories.TaxCategory;
 import io.sphere.sdk.taxcategories.commands.TaxCategoryCreateCommand;
-import io.sphere.sdk.types.Type;
-import io.sphere.sdk.types.TypeDraft;
-import io.sphere.sdk.types.commands.TypeCreateCommand;
-import io.sphere.sdk.types.queries.TypeQuery;
 import models.wrapper.ProductDraftWrapper;
 import models.wrapper.ProductTypeDraftWrapper;
 import models.wrapper.TaxCategoryWrapper;
-import models.wrapper.TypeDraftWrapper;
 import play.Configuration;
 import play.Logger;
 import play.libs.F;
@@ -40,9 +35,6 @@ public class ImportServiceImpl extends AbstractShopService implements ImportServ
     private static final String PRODUCT_TYPE_JSON_RESOURCE = "data/product-type-draft.json";
     private static final String TAX_CATEGORY_JSON_RESOURCE = "data/tax-category-draft.json";
     private static final String PRODUCT_JSON_RESOURCE = "data/product-draft.json";
-    private static final String TYPE_DRAFT_JSON_RESOURCE = "data/type-draft.json";
-
-    private static final String CUSTOM_TYPE_KEY = "cart-frequency-key";
 
     @Inject
     public ImportServiceImpl(final PlayJavaSphereClient playJavaSphereClient, final Configuration configuration) {
@@ -50,9 +42,8 @@ public class ImportServiceImpl extends AbstractShopService implements ImportServ
         requireNonNull(configuration);
         final Boolean importEnabled = configuration.getBoolean("fixtures.import.enabled", false);
         LOG.debug("Import enabled: {}", importEnabled);
-        if(importEnabled) {
+        if (importEnabled) {
             importProductData();
-            importCustomType();
         }
     }
 
@@ -60,7 +51,7 @@ public class ImportServiceImpl extends AbstractShopService implements ImportServ
         LOG.debug("Starting Product import");
         currentProductData().onRedeem(existingProduct -> {
             LOG.debug("Existing Product found: {}", existingProduct.isPresent());
-            if(!existingProduct.isPresent()) {
+            if (!existingProduct.isPresent()) {
                 exportProductModel().onRedeem(product -> LOG.debug("Finished Product import, created '{}'", product));
             }
         });
@@ -74,36 +65,6 @@ public class ImportServiceImpl extends AbstractShopService implements ImportServ
         return productProjectionResultPromise.map(PagedQueryResult::head);
     }
 
-
-    private void importCustomType() {
-        LOG.debug("Starting custom Type import");
-        currentCustomType().onRedeem(currentCustomType -> {
-            LOG.debug("Existing custom Type found: {}", currentCustomType.isPresent());
-            if (!currentCustomType.isPresent()) {
-                exportCustomType().onRedeem(type -> LOG.debug("Finished custom Type import, created '{}'", type.getKey()));
-            }
-        });
-    }
-
-    private F.Promise<Optional<Type>> currentCustomType() {
-        final TypeQuery query = TypeQuery.of();
-        final F.Promise<PagedQueryResult<Type>> execute = playJavaSphereClient().execute(query);
-        return execute.map(typePagedQueryResult ->
-                        typePagedQueryResult.getResults().stream()
-                                .filter(type -> CUSTOM_TYPE_KEY.equals(type.getKey()))
-                                .findFirst()
-        );
-    }
-
-    @Override
-    public F.Promise<Type> exportCustomType() {
-        final TypeDraftWrapper typeDraftWrapper = JsonUtils.readObjectFromResource(TYPE_DRAFT_JSON_RESOURCE,
-                TypeDraftWrapper.class);
-        final TypeDraft typeDraft = typeDraftWrapper.createTypeDraft();
-        final F.Promise<Type> customTypePromise = playJavaSphereClient().execute(TypeCreateCommand.of(typeDraft));
-        return customTypePromise;
-    }
-
     @Override
     public F.Promise<Product> exportProductModel() {
         final F.Promise<TaxCategory> taxCategoryPromise = createTaxCategoryModel();
@@ -114,7 +75,7 @@ public class ImportServiceImpl extends AbstractShopService implements ImportServ
                     ProductDraftWrapper.class);
 
             final ProductDraft productDraft = productDraftWrapper.createProductDraft(productType.toReference(),
-                            taxCategory.toReference());
+                    taxCategory.toReference());
             return playJavaSphereClient().execute(ProductCreateCommand.of(productDraft))
                     .flatMap(product -> playJavaSphereClient().execute(ProductUpdateCommand.of(product, Publish.of())));
         }));
@@ -123,7 +84,8 @@ public class ImportServiceImpl extends AbstractShopService implements ImportServ
     private F.Promise<ProductType> createProductTypeModel() {
         final ProductTypeDraftWrapper productTypeDraftWrapper =
                 JsonUtils.readObjectFromResource(PRODUCT_TYPE_JSON_RESOURCE, ProductTypeDraftWrapper.class);
-        return playJavaSphereClient().execute(ProductTypeCreateCommand.of(productTypeDraftWrapper.createProductTypeDraft()));
+        return playJavaSphereClient()
+                .execute(ProductTypeCreateCommand.of(productTypeDraftWrapper.createProductTypeDraft()));
     }
 
     private F.Promise<TaxCategory> createTaxCategoryModel() {
